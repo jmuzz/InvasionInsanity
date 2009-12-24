ad2e28abcc0cfcdc722507b6ff2799ceba6e7f05
Joel Muzzerall <joel.muzzerall@gmail.com>
2009-12-24 00:01:01 -0800

    Added a basic hex map

--- Classes/GameView.m
+++ Classes/GameView.m
@@ -7,24 +7,43 @@
 
 - (id)initWithFrame:(CGRect)frame {
 	if (self = [super initWithFrame:frame]) {
+		const int NUM_TILES = 3;
+		
+		const int testMap[10][10] = {
+			{2, 2, 2, 1, 2, 2, 0, 0, 0, 0},
+			{2, 2, 2, 1, 2, 2, 0, 0, 0, 0},
+			{2, 2, 1, 2, 2, 2, 2, 0, 0, 0},
+			{1, 1, 1, 2, 2, 2, 2, 2, 0, 0},
+			{2, 1, 2, 2, 2, 2, 2, 2, 0, 2},
+			{2, 1, 2, 2, 2, 2, 2, 0, 0, 2},
+			{2, 2, 1, 2, 2, 2, 2, 0, 2, 2},
+			{2, 2, 1, 1, 2, 2, 2, 2, 0, 2},
+			{2, 2, 2, 1, 2, 2, 2, 2, 0, 2},
+			{2, 2, 2, 2, 1, 2, 2, 2, 0, 2}
+		};
+		
 		CGRect map_frame = [self frame];
 		map_frame.size.height -= 20;
 		map = [[UIView alloc] initWithFrame:map_frame];
-		//map = [[UIView alloc] initWithImage:[UIImage imageNamed:@"player.png"]];
-		//map.center = self.center;
+		UIImage *tileImages = [UIImage imageNamed:@"hexes.png"];
+		CGImageRef tileImageRefs[NUM_TILES];
+		for (int i = 0; i < NUM_TILES; i++) {
+			CGImageRef ir = CGImageCreateCopy([tileImages CGImage]);
+			tileImageRefs[i] = CGImageCreateWithImageInRect(ir, CGRectMake(i*36, 0, 36, 32));
+		}
+
+		CALayer *tile_array[10][10];
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				tile_array[i][j] = [CALayer layer];
+				tile_array[i][j].position = CGPointMake(27.0f * i + 18, 32.0f * j + ((i % 2 == 1) ? 16.0f : 0.0f));
+				tile_array[i][j].bounds = CGRectMake(0.0f, 0.0f, 36.0f, 32.0f);
+				tile_array[i][j].contents = tileImageRefs[testMap[j][i]];
+
+				[[map layer] addSublayer:tile_array[i][j]];
+			}
+		}
 		
-		CALayer *tile = [CALayer layer];
-		tile.position = CGPointMake(50.0f, 50.0f);
-		tile.bounds = CGRectMake(0.0f, 0.0f, 100.0f, 100.0f);
-		UIImage *tile_contents_u = [UIImage imageNamed:@"player.png"];
-		CGImageRef *tile_contents_i = tile_contents_u.CGImage;
-		tile.contents = tile_contents_i;
-		
-		//UIImageView *tile = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"player.png"]];
-		//tile.center = map.center;
-		[[map layer] addSublayer:tile];
-		
-		//[[map layer] addSublayer:tile];
 		[self addSubview:map];
 	}
 	
