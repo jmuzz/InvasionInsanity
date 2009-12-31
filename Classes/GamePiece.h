1bf4ca70284504173386d883c9d762db0a060cfc
Joel Muzzerall <joel.muzzerall@gmail.com>
2009-12-30 19:26:00 -0800

    Added a GamePiece class which will represent a unit on the map

---
+++ Classes/GamePiece.h
@@ -0,0 +1,16 @@
+//
+//  GamePiece.h
+//  InvasionInsanity
+//
+//  Created by Joel on 12/30/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GamePiece : NSObject {
+
+}
+
+@end
---
+++ Classes/GamePiece.m
@@ -0,0 +1,14 @@
+//
+//  GamePiece.m
+//  InvasionInsanity
+//
+//  Created by Joel on 12/30/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "GamePiece.h"
+
+
+@implementation GamePiece
+
+@end
