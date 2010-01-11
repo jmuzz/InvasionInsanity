bac96e0989cb68796ddb9b5d70e02ebccd35699a
Joel Muzzerall <joel.muzzerall@gmail.com>
2010-01-11 02:12:24 -0800

    Fixed a game breaker

--- Classes/GameViewController.m
+++ Classes/GameViewController.m
@@ -144,13 +144,13 @@
 
 				case verifyMoveState:
 					if (!piece) {
-						if ([map pieceCanMoveToHex:hex piece:selectedPiece]) {
+/*						if ([map pieceCanMoveToHex:hex piece:selectedPiece]) {
 							[selectedPiece setCoordsToX:[map hexXFromPoint:tloc] y:[map hexYFromPoint:tloc]];
 							if (selectedPiece.x == oldPieceX && selectedPiece.y == oldPieceY) {
 								gameState = unitSelectedState;
 							}
 							selectedHex = hex;
-						}
+						}*/
 					} else {
 						if ([map pieceCanAttackDefender:piece attacker:selectedPiece]) {
 							defendingPiece = piece;
