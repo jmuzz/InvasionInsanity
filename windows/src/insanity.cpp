1194943df174f2a94e83a59db4bd59124d9bdb54
jmuzz <joel.muzzerall@gmail.com>
2011-04-28 18:30:31 -0400

    It displays a sprite now

--- windows/src/insanity.cpp
+++ windows/src/insanity.cpp
@@ -1,10 +1,30 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-  sf::Window App(sf::VideoMode(800, 600, 32), "SFML Window");
-  App.Display();
-  sf::Sleep(5.f);
+  sf::RenderWindow App(sf::VideoMode(800, 600, 32), "SFML Window");
+  sf::Image Image;
+  if (!Image.LoadFromFile("sprite.png"))
+  {
+    App.Close();
+  }
+  sf::Sprite Sprite;
+  Sprite.SetImage(Image);
+
+  while (App.IsOpened())
+  {
+    sf::Event Event;
+    while (App.GetEvent(Event))
+    {
+      if (Event.Type == sf::Event::Closed)
+        App.Close();
+    }
+
+    App.Clear(sf::Color(200, 0, 0));
+    App.Draw(Sprite);
+
+    App.Display();
+  }
 
   return EXIT_SUCCESS;
 }
